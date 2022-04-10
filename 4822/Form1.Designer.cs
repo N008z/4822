3924ab01e99876e545bf719d663346c6e5a70e2d
SeorangManusiaa <33848095+N008z@users.noreply.github.com>
2022-04-10 18:21:07 +0700

    Add project files.

---
+++ 4822/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _4822
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPlayer = new System.Windows.Forms.DataGridView();
+            this.tbQuery = new System.Windows.Forms.TextBox();
+            this.buttonQuery = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPlayer
+            // 
+            this.dgvPlayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlayer.Location = new System.Drawing.Point(31, 12);
+            this.dgvPlayer.Name = "dgvPlayer";
+            this.dgvPlayer.Size = new System.Drawing.Size(419, 371);
+            this.dgvPlayer.TabIndex = 0;
+            // 
+            // tbQuery
+            // 
+            this.tbQuery.Location = new System.Drawing.Point(495, 12);
+            this.tbQuery.Name = "tbQuery";
+            this.tbQuery.Size = new System.Drawing.Size(393, 20);
+            this.tbQuery.TabIndex = 1;
+            this.tbQuery.TextChanged += new System.EventHandler(this.tbQuery_TextChanged);
+            // 
+            // buttonQuery
+            // 
+            this.buttonQuery.Location = new System.Drawing.Point(495, 50);
+            this.buttonQuery.Name = "buttonQuery";
+            this.buttonQuery.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuery.TabIndex = 2;
+            this.buttonQuery.Text = "RUN";
+            this.buttonQuery.UseVisualStyleBackColor = true;
+            this.buttonQuery.Click += new System.EventHandler(this.buttonQuery_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(601, 50);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "CLEAR";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 410);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonQuery);
+            this.Controls.Add(this.tbQuery);
+            this.Controls.Add(this.dgvPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPlayer;
+        private System.Windows.Forms.TextBox tbQuery;
+        private System.Windows.Forms.Button buttonQuery;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ 4822/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace _4822
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public static string sqlConnection = "server=localhost;uid=root;pwd=;database=premier_league";
+        MySqlConnection sqlConnect = new MySqlConnection(sqlConnection);
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        DataTable table = new DataTable();
+        string sqlQuery;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQuery_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonQuery_Click(object sender, EventArgs e)
+        {
+            sqlConnect.Open();
+            DataTable dtPlayer = new DataTable();
+            sqlQuery = tbQuery.Text;
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtPlayer);
+            dgvPlayer.DataSource = dtPlayer;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            dgvPlayer.DataSource = null;
+            dgvPlayer.Rows.Clear();
+            sqlConnect.Close();
+            dgvPlayer.DataSource = table;
+        }
+    }
+}
